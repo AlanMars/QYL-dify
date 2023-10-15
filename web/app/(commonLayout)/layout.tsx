02610703066ebc7350a25be037059604af895f2e
AlanMars <alan.yin@icloud.com>
2023-10-15 22:32:48 +0800

    Code polish - check ga

--- web/app/(commonLayout)/layout.tsx
+++ web/app/(commonLayout)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { ReactNode } from 'react'
 import SwrInitor from '@/app/components/swr-initor'
 import { AppContextProvider } from '@/context/app-context'
-import GA, { GaType } from '@/app/components/base/ga'
+// import GA, { GaType } from '@/app/components/base/ga'
 import HeaderWrapper from '@/app/components/header/HeaderWrapper'
 import Header from '@/app/components/header'
 import { EventEmitterContextProvider } from '@/context/event-emitter'
@@ -11,7 +11,7 @@ import { ProviderContextProvider } from '@/context/provider-context'
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <GA gaType={GaType.admin} />
+      {/* <GA gaType={GaType.admin} /> */}
       <SwrInitor>
         <AppContextProvider>
           <EventEmitterContextProvider>
