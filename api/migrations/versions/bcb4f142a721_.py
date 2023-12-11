b28dc5eab7579b1b87d5cbc771b464093a787deb
AlanMars <alan.yin@icloud.com>
2023-12-11 14:00:20 +0800

    Update logo and add db migration version file

---
+++ api/migrations/versions/bcb4f142a721_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bcb4f142a721
+Revises: 53a2a2e53893, fca025d3b60f
+Create Date: 2023-12-10 11:12:43.370222
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bcb4f142a721'
+down_revision = ('53a2a2e53893', 'fca025d3b60f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
