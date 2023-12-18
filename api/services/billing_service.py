ed3fa30c0eeec935ac129c66d91aba980215c3b8
AlanMars <alan.yin@icloud.com>
2023-12-18 12:22:34 +0800

    Update billing_info and billing_invoices

--- api/services/billing_service.py
+++ api/services/billing_service.py
@@ -1,6 +1,10 @@
 import os
 import requests
+import logging
+from flask_login import current_user
+from flask_restful import fields, marshal
 
+logger = logging.getLogger(__name__)
 
 class BillingService:
     base_url = os.environ.get('BILLING_API_URL', 'BILLING_API_URL')
@@ -11,7 +15,15 @@ class BillingService:
         params = {'tenant_id': tenant_id}
 
         # billing_info = cls._send_request('GET', '/subscription/info', params=params)
-
+        tenant_fields = {
+            'id': fields.String,
+            'name': fields.String,
+            'plan': fields.String,
+            'status': fields.String,
+            'premium_rate': fields.Float,
+            'role': fields.String
+        }
+        
         billing_info_sandbox = {
             "enabled": True,
             "subscription": {
@@ -64,22 +76,32 @@ class BillingService:
             },
             "members": {
                 "size": 2,
-                "limit": 1000
+                "limit": 0
             },
             "apps": {
                 "size": 9,
-                "limit": 1000
+                "limit": 0
             },
             "vector_space": {
                 "size": 2.87,
-                "limit":5000
+                "limit": 0
             },
             "docs_processing": "standard",
             "can_replace_logo": True
         }
 
-        billing_info = billing_info_team
+        current_tenant = current_user.current_tenant
+        logger.info(f'current_tenant: {marshal(current_tenant, tenant_fields)}')
+        workspace_plan = current_tenant.plan
+        logger.info(f'workspace_plan: {workspace_plan}')
         
+        billing_info = billing_info_sandbox
+        if workspace_plan == 'professional':
+            billing_info = billing_info_professional
+        elif workspace_plan == 'team':
+            billing_info = billing_info_team
+            
+        logger.info(f'billing_info: {billing_info}')
         return billing_info
 
     @classmethod
@@ -110,7 +132,12 @@ class BillingService:
     @classmethod
     def get_invoices(cls, prefilled_email: str = ''):
         params = {'prefilled_email': prefilled_email}
-        return cls._send_request('GET', '/invoices', params=params)
+        invoices_url = f'/billing/invoices/{prefilled_email}'
+        logger.info(f'invoices_url: {invoices_url}')
+        
+        return {"url": invoices_url}
+        # return cls._send_request('GET', '/invoices', params=params)
+        
 
     @classmethod
     def _send_request(cls, method, endpoint, json=None, params=None):
